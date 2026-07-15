94af34bc1bd1482d6d4b0f29afae4915c48f4d72
VladHekolov <75802020+VladHekolov@users.noreply.github.com>
2026-07-15 13:26:59 +0300

    Configure shared VOCAVA public API endpoints

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,10 @@ window.VLADISLOVEX_CONFIG = Object.freeze({
   apiBaseUrl: 'https://api.vocava.ru',
   artistSlug: 'vladislav-hekolov',
   leadEndpoint: '/api/public/leads',
+  artistsEndpoint: '/api/public/artists',
+  repertoireEndpointTemplate: '/api/public/artists/{artistId}/repertoire',
+  publicDataCacheTtlMs: 30000,
+  publicDataRequestTimeoutMs: 5500,
   useVocavaApi: true,
   formSubmitFallbackEnabled: true
 });
