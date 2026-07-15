8f938bdb270f6ddf58a2009f874eb9ce4a54f2b5
VladHekolov <75802020+VladHekolov@users.noreply.github.com>
2026-07-15 13:19:32 +0300

    Add repertoire script entrypoint

---
+++ assets/js/repertoire.js
@@ -0,0 +1,1 @@
+(function(){var s=document.createElement('script');s.src='https://storage.yandexcloud.net/vocava-media/admin/vocava-repertoire.js?v=13';s.defer=true;document.head.appendChild(s);}());
