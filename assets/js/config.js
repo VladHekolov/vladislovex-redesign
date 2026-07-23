05779a496eb47ec4f67b0e1ad6a0f1832b40dace
VladHekolov <75802020+VladHekolov@users.noreply.github.com>
2026-07-23 14:59:02 +0300

    Подключить исправления главной страницы

--- assets/js/config.js
+++ assets/js/config.js
@@ -42,6 +42,15 @@ window.VLADISLOVEX_CONFIG = Object.freeze({
 })();
 
 (function loadVisualScripts() {
+  function loadStyle(href, marker) {
+    if (document.querySelector('link[' + marker + ']')) return;
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = href;
+    link.setAttribute(marker, '');
+    document.head.appendChild(link);
+  }
+
   function loadScript(src, marker, onload) {
     var existing = document.querySelector('script[' + marker + ']');
     if (existing) {
@@ -62,6 +71,8 @@ window.VLADISLOVEX_CONFIG = Object.freeze({
   }
 
   if (document.body && document.body.id === 'top') {
+    loadStyle('/assets/css/home-ui-fixes.css?v=20260723-1', 'data-vh-home-ui-fixes');
+    loadScript('/assets/js/home-ui-fixes.js?v=20260723-1', 'data-vh-home-ui-fixes');
     loadScript('/assets/js/home-copy.js?v=20260716-1', 'data-vh-home-copy');
     loadScript('/assets/js/mobile-cleanup.js?v=20260716-2', 'data-vh-mobile-cleanup');
     loadScript('/assets/js/mobile-benefits-carousel.js?v=20260716-3', 'data-vh-mobile-benefits');
