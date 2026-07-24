42a7243d028546e32e1749e2daf8c38c1ae3bd6b
VladHekolov <75802020+VladHekolov@users.noreply.github.com>
2026-07-24 23:27:25 +0300

    Обновить версию исправлений главной страницы

--- assets/js/config.js
+++ assets/js/config.js
@@ -71,7 +71,7 @@ window.VLADISLOVEX_CONFIG = Object.freeze({
   }
 
   if (document.body && document.body.id === 'top') {
-    loadStyle('/assets/css/home-ui-fixes.css?v=20260723-1', 'data-vh-home-ui-fixes');
+    loadStyle('/assets/css/home-ui-fixes.css?v=20260724-2', 'data-vh-home-ui-fixes');
     loadScript('/assets/js/home-ui-fixes.js?v=20260723-1', 'data-vh-home-ui-fixes');
     loadScript('/assets/js/home-copy.js?v=20260716-1', 'data-vh-home-copy');
     loadScript('/assets/js/mobile-cleanup.js?v=20260716-2', 'data-vh-mobile-cleanup');
