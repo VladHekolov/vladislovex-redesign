4eb624f4845ece42ba20598e311f1a9eb748a7b3
VladHekolov <75802020+VladHekolov@users.noreply.github.com>
2026-07-28 20:31:35 +0300

    Remove obsolete mobile cleanup script

--- assets/js/config.js
+++ assets/js/config.js
@@ -160,7 +160,6 @@ window.VLADISLOVEX_CONFIG = Object.freeze({
     loadStyle('/assets/css/home-ui-fixes.css?v=20260728-1', 'data-vh-home-ui-fixes');
     loadScript('/assets/js/home-ui-fixes.js?v=20260723-1', 'data-vh-home-ui-fixes');
     loadScript('/assets/js/home-copy.js?v=20260716-1', 'data-vh-home-copy');
-    loadScript('/assets/js/mobile-cleanup.js?v=20260716-2', 'data-vh-mobile-cleanup');
   }
 
   loadScript('/assets/vendor/vh-icons.bundle.js?v=20260716-1', 'data-vh-icon-bundle', function () {
